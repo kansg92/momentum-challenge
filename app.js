efa04c57687284229c18b29f3b482ed9acc1bd5b
kansg92 <kansg922@gmail.com>
2022-05-10 22:01:05 +0900

    study object

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const a = 5;
-const b = 2;
-const myName = "kan";
+const player = {
+    name: "kan",
+    points: 10,
+    handsome: true,
+    fat: true,
+};
 
-console.log(a + b);
-console.log(a / b);
-console.log(a * b);
-console.log("hello " + myName);
+console.log(player);
+console.log(player.name);
+
+player.lastname = "potato"; // object 에추가
+player.fat = false; // object 수정
+console.log(player);
