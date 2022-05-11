7d788d8053dab7dcd8a85a9d36d81affca4a0f3a
kansg92 <kansg922@gmail.com>
2022-05-11 22:34:24 +0900

    Searching For Elements (querySelector)

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const title = document.getElementById("title");
 
-title.innerText = "Got you!!";
+// querySelector : css 방식으로 first elements를 찾을 수 있다.
+// querySelectorAll : 모든 elements를 가져온다.
+const title = document.querySelector(".hello h1:first-child"); 
 
-console.log(title.id);
-console.log(title.className);
+
+
+console.log(title);
+
+
+
