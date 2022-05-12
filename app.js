c1b0b0286d3c7e4b514d330fecdc1cec7a9d758b
kansg92 <kansg922@gmail.com>
2022-05-12 17:19:09 +0900

    addEventListener

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
 // querySelector : css 방식으로 first elements를 찾을 수 있다.
 // querySelectorAll : 모든 elements를 가져온다.
-const title = document.querySelector(".hello h1:first-child"); 
+const title = document.querySelector(".hello h1"); 
+
+function handleTitleClick(){
+    console.log("title was clicked!");
+    title.style.color="blue";
+}
 
 
-
-console.log(title);
-
-
-
+title.addEventListener("click", handleTitleClick);
