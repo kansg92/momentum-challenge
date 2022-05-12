5b15e89419f7af09e5c908435af58410c5cf6331
kansg92 <kansg922@gmail.com>
2022-05-12 21:59:46 +0900

    EvenetListener2

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 
 // querySelector : css 방식으로 first elements를 찾을 수 있다.
 // querySelectorAll : 모든 elements를 가져온다.
-const title = document.querySelector(".hello h1"); 
+const h1 = document.querySelector(".hello h1"); 
 
 function handleTitleClick(){
     console.log("title was clicked!");
-    title.style.color="blue";
+    h1.style.color="blue";
+}
+
+function handleMouseEnter(){
+    h1.innerText = "mouse is here!"
+
+}
+
+function handleMouseLeave( ){
+    h1.innerText = "mouse is gone!"
+}
+
+function handleWindowResize(){
+    document.body.style.background = "coral";
+}
+
+function handleWindowCopy(){
+    alert("copier!!")
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI!")
 }
 
 
-title.addEventListener("click", handleTitleClick);
+function handleWindowOnline(){
+    alert("ALL GOOOOOD")
+}
+
+// 2가지 형식으로 함수를 실행 시킬수 있다.
+h1.onclick = handleTitleClick;
+h1.addEventListener("mouseenter",handleMouseEnter);
+h1.addEventListener("mouseleave",handleMouseLeave);
+
+
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
