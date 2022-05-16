09570316835885e3fe107d632a7b1508a1e45fd2
kansg92 <kansg922@gmail.com>
2022-05-17 08:48:42 +0900

    Saving & Loading username

--- app.js
+++ app.js
@@ -12,20 +12,30 @@ const greeting = document.querySelector("#greeting");
 
 // String만 포함된 변수는 관습적으로 대문자로 정의한다.
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    
+    localStorage.setItem(USERNAME_KEY,username);
     // JS에서 String을 작성하는 2가지 방법.
-    greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`;
-
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    console.log(greeting);
+    paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username){
+    greeting.innerText = `Hello! ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
+if (savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else {
+    // show the greeting
+    paintGreeting(savedUsername);
+
+}
