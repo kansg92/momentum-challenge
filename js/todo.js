604cacefe654875b8476374c64bf156017626a08
kansg92 <kansg922@gmail.com>
2022-05-20 21:54:38 +0900

    saving todos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,13 @@ const toDoForm = document.querySelector('#todo-form');
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector('#todo-list');
 
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("todos", JSON.stringify(toDos));
+
+}
+
 function deleteToDo(event){
     // console.dir(event.target.parentElement) path 를통해 어떤 것을 선택했는지 알 수 있다.
     const li = event.target.parentElement;
@@ -22,14 +29,15 @@ function paintToDo(newTodo){
 };
 
 
-
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     // console.log(toDoInput.value); <----- 입력값은 newTodo로 바꾸어 넘어가고, 내부 칸은 초기화됨.
     toDoInput.value = '';
     // console.log("new:" + newTodo, "todo:"+toDoInput.value); ;
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 };
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
